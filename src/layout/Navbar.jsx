2214101de651fc31ca0a0e502bd625bc7d7f9c4f
Bney28 <bney1@outlook.com>
2023-02-10 14:07:14 -0500

    fix: deleted button not functional

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,10 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-md bg-light">
                 <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
+                    
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav">
                             <li className="nav-item">
